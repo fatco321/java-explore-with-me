63c95dd161a50edcdd9ba61fca234a7d48d19b9a
Владимир <fatco@yandex.ru>
2023-02-16 19:16:19 +0300

    stage 3.fix

--- ewm-service/src/main/java/ru/practicum/controller/like/PrivateLikeController.java
+++ ewm-service/src/main/java/ru/practicum/controller/like/PrivateLikeController.java
@@ -1,6 +1,7 @@
 package ru.practicum.controller.like;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 import ru.practicum.dto.EventShortDto;
 import ru.practicum.service.LikeService;
@@ -10,40 +11,47 @@ import java.util.Collection;
 @RestController
 @RequestMapping("/users/{userId}/")
 @RequiredArgsConstructor
+@Slf4j
 public class PrivateLikeController {
     private final LikeService likeService;
 
     @PatchMapping("/like/{eventId}")
     public EventShortDto toLike(@PathVariable Long userId,
                                 @PathVariable Long eventId) {
+        log.info("user with id {} to like event {}", userId, eventId);
         return likeService.toLike(userId, eventId);
     }
 
     @DeleteMapping("/like/{eventId}")
     public void deleteLike(@PathVariable Long userId,
                            @PathVariable Long eventId) {
+        log.info("user {} delete like event {}", userId, eventId);
         likeService.deleteLike(userId, eventId);
     }
 
     @GetMapping("/like")
     public Collection<EventShortDto> getUserLikesEvents(@PathVariable Long userId) {
+        log.info("user with id {} get liked events", userId);
         return likeService.getUserLikesEvents(userId);
     }
 
     @GetMapping("/dislike")
     private Collection<EventShortDto> getUserDislikesEvents(@PathVariable Long userId) {
+        log.info("user with id {} get disliked event", userId);
         return likeService.getUserDislikesEvents(userId);
     }
 
     @PatchMapping("/dislike/{eventId}")
     public EventShortDto toDislike(@PathVariable Long userId,
                                    @PathVariable Long eventId) {
+        log.info("user {} dislike event {}", userId, eventId);
         return likeService.toDislike(userId, eventId);
     }
 
     @DeleteMapping("/dislike/{eventId}")
     public void deleteDisLike(@PathVariable Long userId,
                               @PathVariable Long eventId) {
+        log.info("user {} delete dislike event {}", userId, eventId);
         likeService.deleteDislike(userId, eventId);
     }
 }
--- ewm-service/src/main/java/ru/practicum/controller/like/PublicLikeController.java
+++ ewm-service/src/main/java/ru/practicum/controller/like/PublicLikeController.java
@@ -1,6 +1,7 @@
 package ru.practicum.controller.like;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,6 +18,7 @@ import java.util.Collection;
 @RestController
 @RequestMapping("/raiting")
 @RequiredArgsConstructor
+@Slf4j
 public class PublicLikeController {
     private final LikeService service;
 
@@ -24,12 +26,14 @@ public class PublicLikeController {
     public Collection<UserShortDto> getPopularUsers(
             @PositiveOrZero @RequestParam(defaultValue = "0") int from,
             @Positive @RequestParam(defaultValue = "10") int size) {
+        log.info("public get popular users, param: from: {}, size: {}", from, size);
         return service.getMostPopularEventsInitiator(PageRequest.of(from, size));
     }
 
     @GetMapping("/events")
     public Collection<EventShortDto> getPopularEvents(@PositiveOrZero @RequestParam(defaultValue = "0") int from,
                                                       @Positive @RequestParam(defaultValue = "10") int size) {
+        log.info("public get popular events, param: from: {}, size: {}", from, size);
         return service.getMostPopularEvents(from, size);
     }
 }
